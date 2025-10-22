2e1c3e91a24401c2ef5a1a272b0b8d6325673c6a
Srinath-Rajkumar <srinathrajkumar.ece@gmail.com>
2025-10-22 17:00:32 +0530

    conflit try 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ function App() {
   return (
     <>
       <p>Git cmd test</p>
-      <p>testing conflict here</p>
-      <p>new</p>
-      <p>git reabse test</p>
-      <p>test 3</p>
-      <p>git test stach</p>
-      <p>new change</p>
-      <p>different commit in test</p>
+      <p>git we need conflict</p>
     </>
   );
 }
